5cbad58cb074ae997754b7c64209dc59eaeb9d51
ejevichkin <88722239+ejevichkin@users.noreply.github.com>
2022-08-02 14:29:48 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -13,8 +13,8 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'qabachok@inbox.ru';                 // Наш логин
-$mail->Password = 'iPmKaoo2YA3_';                           // Наш пароль от ящика
+$mail->Username = '                // Наш логин
+$mail->Password = '                          // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
